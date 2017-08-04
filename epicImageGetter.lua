47897d7e70f9f3494bd4d700330dc860cd6ddcff
Philip Linden <runphilrun@users.noreply.github.com>
2017-08-04 14:40:10 -0700

    test lua

---
+++ epicImageGetter.lua
@@ -0,0 +1,16 @@
+  function Initialize()
+    sAPIdata= SELF:GetOption('epicData')
+end
+
+
+
+function Update()
+    sAPIurl= SELF:GetOption('apiURL')
+    sAPIdata= SELF:GetOption('epicData')
+    count = 0
+    for word in string.gmatch(sAPIdata,'image') do
+       count = count + 1
+    end
+    print(count..' images found')
+    return('the url '..sAPIurl..' returned '..sAPIdata..'!')
+end
