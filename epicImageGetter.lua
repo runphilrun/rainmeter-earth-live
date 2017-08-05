5cd702cafe6be0ef53037defe01e226bbb778ac5
runphilrun <pjl7651@rit.edu>
2017-08-05 00:21:26 -0700

    fix api getter

--- epicImageGetter.lua
+++ epicImageGetter.lua
@@ -2,15 +2,13 @@
     sAPIdata= SELF:GetOption('epicData')
 end
 
-
-
 function Update()
     sAPIurl= SELF:GetOption('apiURL')
     sAPIdata= SELF:GetOption('epicData')
     count = 0
-    for word in string.gmatch(sAPIdata,'image') do
+    for word in string.gmatch(sAPIdata,'"image"') do
        count = count + 1
     end
     print(count..' images found')
-    return('the url '..sAPIurl..' returned '..sAPIdata..'!')
+    return(count)
 end
